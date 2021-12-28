7b0877cd414480ba16c3267a5ae0e3db76b49171
Dave Mann <davemanndna@gmail.com>
2021-12-28 20:28:31 +0000

    Client API endpoint and Marshmallow implementation

--- api/main.py
+++ api/main.py
@@ -1,8 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from models import app, Clients
 
-app = Flask(__name__)
 
-@app.route("/client-list")
+
+@app.route("/client-list", methods=["GET"])
 def client_list():
-    
-    return "Hello World"
+    if request.method == "GET":
+        data = Clients.query.all()
+        result = [d.__dict__ for d in data]
+        return jsonify(result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
