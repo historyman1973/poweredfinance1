c6a4f52cee26f695c90a69ac75dd09e01ef9f15c
Dave Mann <davemanndna@gmail.com>
2022-01-23 20:56:36 +0000

    Added Holding and Transaction models

--- api/models.py
+++ api/models.py
@@ -1,13 +1,16 @@
 from app import db, ma
 from flask_sqlalchemy import SQLAlchemy
 
-investment_instrument = db.Table('investment_instrument',
-                                 db.Column('investment_id', db.Integer,
-                                           db.ForeignKey('investment.id')),
-                                 db.Column('instrument_id', db.Integer,
-                                           db.ForeignKey('instrument.id'))
-                                 )
+investment_holdings = db.Table('investment_holdings', db.Column('investment_id', db.Integer, db.ForeignKey('investment.id')),
+                               db.Column('holding_id', db.Integer, db.ForeignKey('holding.id')))
 
+
+holdings_transactions = db.Table('holdings_transactions',
+                                 db.Column('holding_id', db.Integer,
+                                           db.ForeignKey('holding.id')),
+                                 db.Column('transaction_id', db.Integer,
+                                           db.ForeignKey('transaction.id'))
+                                 )
 
 class Client(db.Model):
     __tablename__ = "client"
@@ -35,6 +38,31 @@ client_schema = ClientSchema()
 clients_schema = ClientSchema(many=True)
 
 
+class Instrument(db.Model):
+    __tablename__ = "instrument"
+
+    id = db.Column(db.Integer, primary_key=True)
+    symbol = db.Column(db.String)
+    exchange = db.Column(db.String)
+
+    def __init__(self, **kwargs):
+        super(Instrument, self).__init__(**kwargs)
+
+
+class InstrumentSchema(ma.Schema):
+    class Meta:
+        model = Instrument
+        fields = (
+            'id',
+            'symbol',
+            'exchange'
+        )
+
+
+instrument_schema = InstrumentSchema()
+instruments_schema = InstrumentSchema(many=True)
+
+
 class Investment(db.Model):
     __tablename__ = "investment"
 
@@ -47,10 +75,12 @@ class Investment(db.Model):
     owner1_id = db.Column(db.Integer, db.ForeignKey('client.id'))
     owner2_id = db.Column(db.Integer, db.ForeignKey('client.id'))
 
-    instruments = db.relationship('Instrument', secondary=investment_instrument, backref='investments')
+    holdings = db.relationship(
+        'Holding', secondary=investment_holdings, backref='investments')
 
     def __init__(self, **kwargs):
         super(Investment, self).__init__(**kwargs)
+
 
 class InvestmentSchema(ma.Schema):
     class Meta:
@@ -71,30 +101,46 @@ investment_schema = InvestmentSchema()
 investments_schema = InvestmentSchema(many=True)
 
 
-class Instrument(db.Model):
-    __tablename__ = "instrument"
+class Holding(db.Model):
+    __tablename__ = "holding"
 
     id = db.Column(db.Integer, primary_key=True)
-    symbol = db.Column(db.String)
-    exchange = db.Column(db.String)
+    investment_id = db.Column(db.Integer, db.ForeignKey('investment.id'))
+    instrument_id = db.Column(db.Integer, db.ForeignKey('instrument.id'))
     units = db.Column(db.Float)
-    cost = db.Column(db.DECIMAL(asdecimal=False))
+
+    transactions = db.relationship(
+        'Transaction', secondary=holdings_transactions, backref='holdings')
 
     def __init__(self, **kwargs):
-        super(Instrument, self).__init__(**kwargs)
+        super(Holding, self).__init__(**kwargs)
 
-
-class InstrumentSchema(ma.Schema):
+class HoldingSchema(ma.Schema):
     class Meta:
-        model = Instrument
-        fields = (
-            'id',
-            'symbol',
-            'exchange',
-            'units',
-            'cost'
-        )
+        model = Holding
+        fields = ('id', 'investment_id', 'instrument_id', 'units')
+
+holding_schema = HoldingSchema()
+holdings_schema = HoldingSchema(many=True)
 
 
-instrument_schema = InstrumentSchema()
-instruments_schema = InstrumentSchema(many=True)
+class Transaction(db.Model):
+    __tablename__ = "transaction"
+
+    id = db.Column(db.Integer, primary_key=True)
+    ttype = db.Column(db.String)
+    tdate = db.Column(db.Date)
+    units = db.Column(db.Float)
+    price = db.Column(db.DECIMAL(asdecimal=False))
+
+    def __init__(self, **kwargs):
+        super(Transaction, self).__init__(**kwargs)
+
+
+class TransactionSchema(ma.Schema):
+    class Meta:
+        model = Transaction
+        fields = ('id', 'ttype', 'tdate', 'units', 'price')
+
+transaction_schema = TransactionSchema()
+transactions_schema = TransactionSchema(many=True)
