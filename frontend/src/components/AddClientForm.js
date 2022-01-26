0855bd54127eeb9a8fa776c6e21041f3d00a9846
spudg <itzsymbaa@outlook.com>
2022-01-26 20:40:59 +0000

    Split radio group into custom component

--- frontend/src/components/AddClientForm.js
+++ frontend/src/components/AddClientForm.js
@@ -1,17 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { FormControl, FormControlLabel, FormLabel, Grid, Radio, RadioGroup, TextField } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import { FormControl, FormControlLabel, FormLabel, Grid, Radio, TextField } from '@mui/material';
 import { useForm, Form } from './useForm';
-import Input from './controls/Input'
+import Controls from './controls/Controls';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    '& .MuiFormControl-root': {
-      width: '40%',
-      margin: '10px'
-    }
-  }
-}))
+const genderItems = [
+  { id: 'male', title: 'Male' },
+  { id: 'female', title: 'Female' },
+  { id: 'other', title: 'Other' }
+]
+
 
 const initialFValues = {
   id: 0,
@@ -35,45 +32,41 @@ export default function AddClientForm() {
     <Form>
         <Grid container>
           <Grid item xs={6}>
-            <Input
+            <Controls.Input
             name="forename"
             label="Forename"
             value={values.forename}
             onChange={handleInputChange} />
-            <Input
+            <Controls.Input
             label="Preferred name"
             name="preferred_name"
             value={values.preferred_name}
             onChange = {handleInputChange} />
-            <Input
+            <Controls.Input
             label="Middle name(s)"
             name="middle_names"
             value={values.middle_names}
             onChange = {handleInputChange} />
-            <Input
+            <Controls.Input
             label="Surname"
             name="surname"
             value={values.surname}
             onChange = {handleInputChange} />
-            <Input
+            <Controls.Input
             label="isPrimary"
             name="isPrimary"
             value={values.isPrimary}
             onChange = {handleInputChange} />
           </Grid>
           <Grid item xs={6}>
-            <FormControl>
-              <FormLabel>Gender</FormLabel>
-              <RadioGroup 
-              row
-              name="gender"
-              value={values.gender}
-              onChange = {handleInputChange}>
-                <FormControlLabel value="male" control={<Radio />} label="Male" /> 
-                <FormControlLabel value="female" control={<Radio />} label="Female" /> 
-                <FormControlLabel value="other" control={<Radio />} label="Other" /> 
-              </RadioGroup>
-            </FormControl>
+            <Controls.RadioGroup
+             name="gender"
+             label="Gender"
+             value={values.gender}
+             onChange = {handleInputChange}
+             items={ genderItems } />
+            
+
           </Grid>
         </Grid>
     </Form>
---
+++ frontend/src/components/controls/Controls.js
@@ -0,0 +1,9 @@
+import Input from "./Input";
+import RadioGroup from "./RadioGroup";
+
+const Controls = {
+    Input,
+    RadioGroup
+}
+
+export default Controls;
