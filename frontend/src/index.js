afa156db140f2727fb731814c939e8feac7f32f7
spudg <itzsymbaa@outlook.com>
2022-01-30 13:03:20 +0000

    Fixed styling by re-ordering bootstrap/custom css imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './css/index.css';
 import { BrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout';
 import { ToastContainer } from 'react-bootstrap';
-import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
