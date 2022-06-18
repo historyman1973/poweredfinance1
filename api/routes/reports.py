ce446546ad110600237cec62fe98c2f9967d6e5d
Dave Mann <davemanndna@gmail.com>
2022-06-18 18:54:25 +0100

    Basic client summary report added

--- api/routes/reports.py
+++ api/routes/reports.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, make_response
 import pdfkit
-from models import Client
+from models import Client, Investment, Property, Liability, OtherAsset
 from routes.clients import get_networth
 
 reports_blueprint = Blueprint('reports_blueprint', __name__)
@@ -30,6 +30,36 @@ def mi_client_list():
 
     response = make_response(pdf)
     response.headers["Content-Type"] = 'application/pdf'
-    response.headers["Content-Disposition"] = 'inline; filename=client_list.pdf'
+    response.headers["Content-Disposition"] = 'inline; filename="%client_list"%.pdf'
+    response.headers["Content-Orientation"] = 'landscape'
+
+    return response
+
+@reports_blueprint.route("/client-summary/<client_id>")
+def client_summary(client_id):
+
+    client = Client.query.get(client_id)
+    if client.isPrimary == 1:
+        investments = Investment.query.filter(Investment.owner1_id == client_id)
+        otherassets = OtherAsset.query.filter(OtherAsset.owner1_id == client_id)
+        properties = Property.query.filter(Property.owner1_id == client_id)
+        liabilities = Liability.query.filter(Liability.owner1_id == client_id)
+    else:
+        investments = Investment.query.filter(Investment.owner2_id == client_id)
+        otherassets = OtherAsset.query.filter(OtherAsset.owner2_id == client_id)
+        properties = Property.query.filter(Property.owner2_id == client_id)
+        liabilities = Liability.query.filter(Liability.owner2_id == client_id)
+
+    
+    report = render_template("./client-summary.html", 
+        client=client, investments=investments, otherassets=otherassets, properties=properties, liabilities=liabilities)
+    pdf = pdfkit.from_string(report, False)
+
+    filename = str(client.forename + " " + client.surname + " (" + str(client.id) + ") - Client Summary.pdf")
+
+    response = make_response(pdf)
+    response.headers["Content-Type"] = 'application/pdf'
+    response.headers["Content-Disposition"] = 'attachment; filename=' + filename
+    response.headers["Content-Orientation"] = 'portrait'
 
     return response
