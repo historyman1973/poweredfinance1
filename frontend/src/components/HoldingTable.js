d6888183f7e2c52bfb431d3c1cae3357ad495fbc
spudg <itzsymbaa@outlook.com>
2022-06-26 18:58:36 +0100

    Transaction table added under new overview - some issues to fix including price, date format

--- frontend/src/components/HoldingTable.js
+++ frontend/src/components/HoldingTable.js
@@ -1,9 +1,33 @@
 import { DataGrid } from "@mui/x-data-grid";
 import React from "react";
-import { Link } from "@mui/material";
+import {
+  AppBar,
+  Dialog,
+  Link,
+  Slide,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { currencyFormat } from "../components/GlobalFunctions";
+import { Button } from "@mui/material";
+import { ThemeProvider } from "@mui/styles";
+import TransactionOverview from "../TransactionOverview";
+import axios from "axios";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 function HoldingTable({ holdings }) {
+  const [openViewTransactions, setOpenViewTransactions] = React.useState(false);
+  const handleViewTransactionsClose = () => setOpenViewTransactions(false);
+  const [holdingId, setHoldingId] = React.useState(false);
+
+  const handleClick = (id) => {
+    setHoldingId(id);
+    setOpenViewTransactions(true);
+  };
+
   const columns = [
     { field: "id", headerName: "ID", width: 100 },
     { field: "instrument_id", headerName: "Instrument ID", width: 100 },
@@ -15,18 +39,27 @@ function HoldingTable({ holdings }) {
     { field: "units", headerName: "Units", width: 130 },
     { field: "value", headerName: "Value", width: 180 },
     {
-      field: "view",
-      headerName: "View",
+      field: "viewSecurity",
+      headerName: "View Security",
       width: 150,
       renderCell: (params) => (
-        <Link href={`/security/${params.value}`}>View</Link>
+        <Link href={`/security/${params.value}`}>View Security</Link>
+      ),
+    },
+    {
+      field: "viewTransactions",
+      headerName: "View Transactions",
+      width: 150,
+      renderCell: (params) => (
+        <Button onClick={() => handleClick(params.row.id)}>
+          View Transaction
+        </Button>
       ),
     },
   ];
 
   const returnTable = () => {
     const rows = [];
-    console.log(holdings);
     holdings.map((holding) =>
       rows.push({
         id: holding.holding_id,
@@ -34,7 +67,8 @@ function HoldingTable({ holdings }) {
         instrument_name: holding.instrument_name,
         units: holding.current_units,
         value: currencyFormat(parseFloat(holding.current_value)),
-        view: holding.holding_ticker,
+        viewSecurity: holding.holding_ticker,
+        viewTransactions: holding.holding_id,
       })
     );
     return <DataGrid rows={rows} columns={columns} />;
@@ -50,6 +84,33 @@ function HoldingTable({ holdings }) {
       }}
     >
       {returnTable()}
+      <Dialog
+        fullScreen
+        open={openViewTransactions}
+        onClose={handleViewTransactionsClose}
+        TransitionComponent={Transition}
+      >
+        <ThemeProvider>
+          <AppBar
+            sx={{ position: "relative" }}
+            style={{ background: "#ff00ff" }}
+          >
+            <Toolbar>
+              <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+                Viewing transactions for holding with ID {holdingId}
+              </Typography>
+              <Button
+                autoFocus
+                color="inherit"
+                onClick={handleViewTransactionsClose}
+              >
+                Close
+              </Button>
+            </Toolbar>
+          </AppBar>
+          <TransactionOverview holdingId={holdingId} />
+        </ThemeProvider>
+      </Dialog>
     </div>
   );
 }
